d7b1a8dbd76fca2ccaab3fd88d7a192022addde2
alexisw2025-lgtm <alexisw2025@gmail.com>
2026-06-22 11:57:43 -0400

    Add files via upload

--- src/SupplementsPage.jsx
+++ src/SupplementsPage.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import React, { useState } from "react";
 import { useLang } from "./LangContext";
 
 // Merge English base supplement data with its own inline translations block.
